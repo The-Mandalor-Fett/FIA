a4529286d2ac19395d0915c4df6ff4a9b21d8a5b
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-03-03 01:12:00 -0600

    Agregamos la impresión del plan

--- Ejercicio 1/segundo_programa.py
+++ Ejercicio 1/segundo_programa.py
@@ -1,10 +1,11 @@
 # PROGRAMA 2. VALIDA QUE NO SE REPITAN LAS ESTACIONES
 import csv
+from tokenize import PlainToken
 
 # CARGA ESTACIONES
 estacion = []  # INICIALIZA LA LISTA
 estacion.append("vacio")  # COLOCA EL ELEMENTO EN EL INDICE CERO
-with open("estaciones2.csv", encoding = "utf8" ,newline='') as File:  # ABRE EL ARCHIVO
+with open("estaciones.csv", encoding = "utf8" ,newline='') as File:  # ABRE EL ARCHIVO
     archivo = csv.reader(File)  # INTERPRETA EL ARCHIVO COMO CSV
     for registro in archivo:  # TOMA UN REGISTRO DEL ARCHIVO CSV
 
@@ -19,7 +20,7 @@ with open("estaciones2.csv", encoding = "utf8" ,newline='') as File:  # ABRE EL 
        
         # AÑADE LA TUPLA EN LA LISTA estacion
         estacion.append(tupla)
-        print(str(tupla))     # DESPLIEGA LA TUPLA
+        #print(str(tupla))     # DESPLIEGA LA TUPLA
 
 # PIDE LA ESTACION INICIAL Y LA FINAL
 estacionIni = int(input('Escribe la estación inicial: '))
@@ -38,7 +39,7 @@ while len(cola) > 0: # MIENTRAS LA COLA TENGA ELEMENTOS
    
     print("Utilizo: "+str(estacion[nodo][1])) # DESPLIEGA NOMBRE DE ESTACIÓN
 
-    utilizados.append(nodo)  # AÑADE EL NODO A LOS UTILIZADOS PARA EVITAR LOOPS INFINITO
+    utilizados.append(int(nodo))  # AÑADE EL NODO A LOS UTILIZADOS PARA EVITAR LOOPS INFINITO
 
     visitados.append((anterior,nodo)) # GUARDA PARES (NODO anterior, nodo ACTUAL)
     anterior = nodo   # ACTUALIZA EL NODO anterior COMO EL nodo ACTUAL
@@ -58,3 +59,14 @@ if haySolucion == True:
     print(visitados) #POR EL MOMENTO IMPRIME LOS DATOS ERRONEOS YA QUE PASA POR PANTITLAN VARIAS VECES AL SER VECINOS
 else:
     print("no hay solución")
+#AHORA IMPRIMIMOS EL PLAN
+print("Estacion de inicio: "+str(estacion[estacionIni][1]))
+print("Estacion a la que se quiere llegar: "+str(estacion[estacionFin][1]))
+#print(type (utilizados))
+# EL plan SE EMPIEZA A IMPRIMIR
+print("Estación || Línea")
+for incremental in utilizados:
+    plan=(estacion[incremental][1],estacion[incremental][2])
+    print("De la estacion "+str(plan),end= " ")
+
+#print(type (plan)) #IMPRIMIE EL tipo de dado DE PLAN (ES UNA TUPLA)
