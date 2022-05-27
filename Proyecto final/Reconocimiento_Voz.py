f32dd45b0c9119fbe93c42cf002fb05524494d59
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-05-26 21:59:00 -0500

    Imprimimos los micrófonos que se encuentre

--- Proyecto final/Reconocimiento_Voz.py
+++ Proyecto final/Reconocimiento_Voz.py
@@ -2,4 +2,7 @@ import speech_recognition as sr
 import webbrowser as wb
 
 def Reconocer_voz():
-    sr.Microphone(device_index = 0)
+    sr.Microphone(device_index = 0) #INICIALIZA EL MICRÓFONO 0 = POR DEFECTO
+    print("Micrfono encontrado en la PC:  \n {sr.Microphone.list_micophpne_names()}")
+    
+
