1f75ec7f08c6ecb06a93d714550acaf7a18c52e6
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-05-27 01:40:09 -0500

    Arreglamos los recuadros

--- Proyecto final/Reconocimiento_ojo.py
+++ Proyecto final/Reconocimiento_ojo.py
@@ -1,8 +1,7 @@
-from pickle import TRUE
 import cv2
 
 #VIDEOCAPTURA
-captura = cv2.VideoCapture(0)
+captura = cv2.VideoCapture(1)
 captura.set(3,1280)
 captura.set(4,720)
 
@@ -23,14 +22,14 @@ while True :
     y1 = int(alto / 3)
     y2 = int(y1 * 2)
 
-    cv2.putText(frame,"Porfavor coloca tu ojo en el rectángulo",(x1 - 50, y1 - 50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-    cv2.rectangle(frame,(x1,x2),(y1,y2),(0,255,0),2)
+    cv2.putText(frame,"Porfavor coloca tu ojo en el rectangulo",(x1 - 50, y1 - 50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
 
     #RECORTE DE NUESTRA ZON DE INTERÉS
     recorte = frame[y1:y2, x1:x2]
 
     gris = cv2.cvtColor(recorte, cv2.COLOR_BGR2GRAY)
-    gris = cv2.GaussianBlur(gris, (3,3),0)
+    gris = cv2.GaussianBlur(gris, (7,7),0)
 
     #SE USA UN UMBRAL PARA DETECTAR LA PUPILA GRACIAS AL NEGRO DE NUESTRA PUPILA
     _ , umbral = cv2.threshold(gris , 7, 255, cv2.THRESH_BINARY_INV)
@@ -39,14 +38,14 @@ while True :
     contornos , _ = cv2.findContours(umbral, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     #ORGANIZAMOS LOS CONTORNOS
-    contornos =  sorted(contornos, key = lambda x: cv2.contourArea(x), reverse = TRUE)
+    contornos =  sorted(contornos, key = lambda x: cv2.contourArea(x), reverse = True)
 
     #DIBUJA CONTORNOS
     for contorno in contornos:
-        (x, y, ancho, alto) = cv2.boundingRect(contorno)
+        (x, y, ancho1, alto1) = cv2.boundingRect(contorno)
 
         #DIBUJA
-        cv2.rectangle(frame,(x + x1, y + y1),(x + ancho + x1, y + alto + y1),(0,255,0),1)
+        cv2.rectangle(frame, (x + x1, y + y1), (x + ancho1 + x1, y + alto1 + y1),(0,255,0),1)
 
         break
     
@@ -57,7 +56,7 @@ while True :
     tecla = cv2.waitKey(1)
     if tecla == 27:
         break
-    captura.release()
-    cv2.destroyAllWindows()
+captura.release()
+cv2.destroyAllWindows()
 
 
