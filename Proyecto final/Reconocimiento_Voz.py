e7561c0ed7319c680853ef089718c976a880c4f6
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-05-26 22:38:39 -0500

    Se agregó el idioma español MX

--- Proyecto final/Reconocimiento_Voz.py
+++ Proyecto final/Reconocimiento_Voz.py
@@ -6,7 +6,7 @@ def Reconocer_voz():
     print("Micrfono encontrado en la PC:  \n {sr.Microphone.list_micophpne_names()}")
     #CREAMOS UN OBJETO DE RECONOCIMIENTO
     reconocimiento = sr.Recognizer()
-    reconocimiento.energy_threshold = 300 #TENEMOS VALORES DE 150 A 3500, PONEMOS EL QUE QUEDE
+    reconocimiento.energy_threshold = 1000 #TENEMOS VALORES DE 150 A 3500, PONEMOS EL QUE QUEDE
     reconocimiento.dynamic_energy_threshold = False
 
     with sr.Microphone() as source:
@@ -14,7 +14,7 @@ def Reconocer_voz():
         reconocimiento.adjust_for_ambient_noise(source) #REDUCE EL RUIDO AMBIENTAL
         audioDelMicrofono = reconocimiento.listen(source) #TOMA LA VOZ DEL MICRÓFONO
         try:
-            frase = reconocimiento.recognize_google(audio)
+            frase = reconocimiento.recognize_google(audioDelMicrofono, language = "es-MX")
             print(f"Dijiste algo como: {frase} ?")
             url = "https://www.google.com/search?q="
             buscar  = url + frase
@@ -28,7 +28,7 @@ def Reconocer_voz():
         except LookupError:
             print("No entendí lo que quisiste decir")
         else:
-            print("Your results will appear in the default browser. Good bye for now...")
+            print("Tu resultado se mostrará en un momento. Adiós :) ")
 
 
 Reconocer_voz()
