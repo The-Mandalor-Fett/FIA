dfc7731f30a9de7cad2a40e7f36691a8ee33d4a5
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-05-29 12:11:27 -0500

    Modificamos la zona de interés

--- Proyecto final/Reconocimiento_ojo.py
+++ Proyecto final/Reconocimiento_ojo.py
@@ -1,9 +1,10 @@
 import cv2
+import numpy as py
 
 #VIDEOCAPTURA
 captura = cv2.VideoCapture(1)
-captura.set(3,1280)
-captura.set(4,720)
+#captura.set(3,1280)
+#captura.set(4,720)
 
 while True :
     #REALIZA LECTURA DE VIDEOCAPTURA Y SI LA CAPTURA ES EXITOSA
@@ -22,17 +23,17 @@ while True :
     y1 = int(alto / 3)
     y2 = int(y1 * 2)
 
+
+    #RECORTE DE NUESTRA ZONA DE INTERÉS
+    zonaDeInteres = frame[y1:y2, x1:x2]
+
+    zonaInteresGris = cv2.cvtColor(zonaDeInteres, cv2.COLOR_BGR2GRAY)
+    zonaInteresGris = cv2.GaussianBlur(zonaInteresGris, (5,5),0)
+
+    '''cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
     cv2.putText(frame,"Porfavor coloca tu ojo en el rectangulo",(x1 - 50, y1 - 50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-    cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
-
-    #RECORTE DE NUESTRA ZON DE INTERÉS
-    recorte = frame[y1:y2, x1:x2]
-
-    gris = cv2.cvtColor(recorte, cv2.COLOR_BGR2GRAY)
-    gris = cv2.GaussianBlur(gris, (7,7),0)
-
     #SE USA UN UMBRAL PARA DETECTAR LA PUPILA GRACIAS AL NEGRO DE NUESTRA PUPILA
-    _ , umbral = cv2.threshold(gris , 7, 255, cv2.THRESH_BINARY_INV)
+    _ , umbral = cv2.threshold(zonaInteresGris , 7, 255, cv2.THRESH_BINARY_INV)
 
     #EXTRAEMOS LOS CONTORNOS DE LA ZONA DE LA PUPILA
     contornos , _ = cv2.findContours(umbral, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -48,12 +49,12 @@ while True :
         cv2.rectangle(frame, (x + x1, y + y1), (x + ancho1 + x1, y + alto1 + y1),(0,255,0),1)
 
         break
+    cv2.imshow("Umbral", umbral)'''
     
     cv2.imshow("Ojos", frame)
-    cv2.imshow("Recorte", recorte)
-    cv2.imshow("Umbral", umbral)
+    cv2.imshow("Recorte", zonaDeInteres)
 
-    tecla = cv2.waitKey(1)
+    tecla = cv2.waitKey(30)
     if tecla == 27:
         break
 captura.release()
