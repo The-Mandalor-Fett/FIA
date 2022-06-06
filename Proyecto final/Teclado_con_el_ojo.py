623fe4d34f70e2e44951d6657ae03bcd01acf49a
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-06-05 23:26:00 -0500

    Agregacmos el teclado virtual al proyecto principal

--- Proyecto final/Teclado_con_el_ojo.py
+++ Proyecto final/Teclado_con_el_ojo.py
@@ -6,12 +6,22 @@ import imutils as nimu
 import dlib as db
 import math
 
+
 captura = cv2.VideoCapture(0)
 #DETECTOR DE LA CARA DE DLIB
 detectorCara = db.get_frontal_face_detector()
 #PREDICTOR DE 68 PUNTOS DE REFERENCIA
 predictor = db.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
+#CREAMOS EL TECLADO
+teclado = np. zeros((1000, 1200, 3), np.uint8)
+key_caps ={0: "Q", 1: "W", 2: "E", 3: "R", 4: "T", 5: "Y", 
+           6: "U", 7: "I", 8: "O", 9: "P", 10: "A", 11: "S", 12: "D",
+           13: "F", 14: "G", 15: "H", 16: "J", 17: "K", 18: "L", 19: "Ñ",
+           20: "Z", 21: "X", 22: "C", 23: "V", 24: "B", 25: "N", 26: "M",}
+
+frames = 0
+index_teclas = 0
 font = cv2.FONT_HERSHEY_PLAIN
 
 def obtener_Parpadeo(puntosOjo, puntosDeCara):
@@ -83,15 +93,124 @@ def mirada(puntosOjo, puntosDeCara):
 def enmedio(p1, p2):
     return(int((p1.x + p2.x)/2), int((p1.y + p2.y)/2))
 
+def letras(index_letra, letra, blanco_tecla):
+    if index_letra == 0:
+        x = 0
+        y = 0
+    elif index_letra == 1:
+        x = 200
+        y = 0
+    elif index_letra == 2:
+        x = 400
+        y = 0
+    elif index_letra == 3:
+        x = 600
+        y = 0
+    elif index_letra == 4:
+        x = 800
+        y = 0
+    elif index_letra == 5:
+        x = 1000
+        y = 0
+    elif index_letra == 6:
+        x = 0
+        y = 200 
+    elif index_letra == 7:
+        x = 200
+        y = 200
+    elif index_letra == 8:
+        x = 400
+        y = 200
+    elif index_letra == 9:
+        x = 600
+        y = 200
+    elif index_letra == 10:
+        x = 800
+        y = 200 
+    elif index_letra == 11:
+        x = 1000
+        y = 200
+    elif index_letra == 12:
+        x = 0
+        y = 400
+    elif index_letra == 13:
+        x = 200
+        y = 400
+    elif index_letra == 14:
+        x = 400
+        y = 400
+    elif index_letra == 15:
+        x = 600
+        y = 400 
+    elif index_letra == 16:
+        x = 800
+        y = 400
+    elif index_letra == 17:
+        x = 1000
+        y = 400
+    elif index_letra == 18:
+        x = 0
+        y = 600
+    elif index_letra == 19:
+        x = 200
+        y = 600 
+    elif index_letra == 20:
+        x = 400
+        y = 600
+    elif index_letra == 21:
+        x = 600
+        y = 600
+    elif index_letra == 22:
+        x = 800
+        y = 600
+    elif index_letra == 23:
+        x = 1000
+        y = 600
+    elif index_letra == 24:
+        x = 0
+        y = 800 
+    elif index_letra == 25:
+        x = 200
+        y = 800
+    elif index_letra == 26:
+        x = 400
+        y = 800
+
+    #TECLAS
+    alto = 200
+    ancho = 200
+    grueso = 3
+    
+    if letra_blanco is True:
+        cv2.rectangle(teclado,(x + grueso , y + grueso), (x + ancho - grueso, y + alto - grueso), (255,255,255), -1)
+    else:
+        cv2.rectangle(teclado,(x + grueso , y + grueso), (x + ancho - grueso, y + alto - grueso), (0,255,0), grueso)
+
+    #FORMATO DE LAS TECLAS
+    escala_letra = 10
+    letra_grueso = 4
+    tamanio = cv2.getTextSize(letra, cv2.FONT_HERSHEY_PLAIN, escala_letra, letra_grueso)[0]
+    texto_ancho , texto_alto = tamanio[0], tamanio[1]
+    texto_x = int((ancho - texto_ancho)/2) + x
+    text_y = int((alto + texto_alto)/2) + y
+    cv2.putText(teclado, letra, (texto_x,text_y), cv2.FONT_HERSHEY_PLAIN, escala_letra, (0,255,0), letra_grueso)
+
+
+
 
 while True:
     #REALIZA LECTURA DE VIDEOCAPTURA Y QUE LA CAPTURA ES EXITOSA
     ret,frame = captura.read()
-
+    #CREAMOS UN NUEVO RECUADRO PARA MOSTRAR DONDE MIRAMOS
+    nuevo_frame = np.zeros((500,500,3),np.uint8)
     #MODIFICA EL TAMAÑO DE LA VENTANA
     frame = nimu.resize(frame,width=720)
     frameGris =  cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#EN ESTA SE APLICA EL DETECTOR FACIAL
     cuadrosDelimitadores = detectorCara(frameGris,1)#OBTIENE LOS CUADROS DELIMITADORES DEL ROSTRO
+    
+    frames += 1
+    teclado[:] = (0,0,0)
+
     for cara in cuadrosDelimitadores:
         codr_x, cord_y = cara.left(), cara.top()
         codr_x1, cord_y1 = cara.right(), cara.bottom()
@@ -112,8 +231,7 @@ while True:
         mirada_ojo_der = mirada([42,43,44,45,46,47], puntosCara)
         miradaOjos = (mirada_ojo_izq + mirada_ojo_der)/2
 
-        #MUESTRA LA DIRECCIÓN
-        nuevo_frame = np.zeros((500,500,3),np.uint8)
+        
 
         if miradaOjos <= 1:
             cv2.putText(frame, "Derecha", (50,100), font, 2, (0,255,0), 3)
@@ -124,11 +242,6 @@ while True:
         else:
             cv2.putText(frame, "Izquierda", (50,100), font, 2, (0,255,0), 3)
             nuevo_frame[:] = (255,0,0)
-
-        cv2.imshow("Nuevo frame", nuevo_frame)
-        
-        
-
         
         #umbralOjo = cv2.resize(umbralOjo, None, fx = 4, fy = 4)
         #ojo = cv2.resize(ojoGris, None, fx = 4, fy = 4)
@@ -141,8 +254,24 @@ while True:
         '''for i in range (0, 68):
             x , y = puntosCara.part(i).x, puntosCara.part(i).y
             cv2.circle(frame, (x, y),2, (0,0,255),-1)'''
-    cv2.imshow("Video",frame)
+    
+    if frames == 10:
+        index_teclas += 1
+        frames = 0
+        
+    if index_teclas == 27:
+        index_teclas = 0
 
+    for i in range(27):
+        if i == index_teclas:
+            letra_blanco = True
+        else:
+            letra_blanco = False
+        letras(i, key_caps[i], letra_blanco)
+
+    cv2.imshow("Video",frame)
+    cv2.imshow("Nuevo frame", nuevo_frame)
+    cv2.imshow("Teclado", teclado)
     #SI LA LECTURA DE LA CAPTURA ES CORRECTA NO CIERRA EL PROGRAMA
     if ret == False:
         break
--- Proyecto final/teclado_virtual.py
+++ Proyecto final/teclado_virtual.py
@@ -1,7 +1,123 @@
 import cv2 
 import numpy as np
 
-teclado = np. zeros((1000, 1500, 3), np.uint8)
+teclado = np. zeros((1000, 1200, 3), np.uint8)
+key_caps ={0: "Q", 1: "W", 2: "E", 3: "R", 4: "T", 5: "Y", 
+           6: "U", 7: "I", 8: "O", 9: "P", 10: "A", 11: "S", 12: "D",
+           13: "F", 14: "G", 15: "H", 16: "J", 17: "K", 18: "L", 19: "Ñ",
+           20: "Z", 21: "X", 22: "C", 23: "V", 24: "B", 25: "N", 26: "M",}
+
+def letras(index_letra, letra, blanco_tecla):
+    if index_letra == 0:
+        x = 0
+        y = 0
+    elif index_letra == 1:
+        x = 200
+        y = 0
+    elif index_letra == 2:
+        x = 400
+        y = 0
+    elif index_letra == 3:
+        x = 600
+        y = 0
+    elif index_letra == 4:
+        x = 800
+        y = 0
+    elif index_letra == 5:
+        x = 1000
+        y = 0
+    elif index_letra == 6:
+        x = 0
+        y = 200 
+    elif index_letra == 7:
+        x = 200
+        y = 200
+    elif index_letra == 8:
+        x = 400
+        y = 200
+    elif index_letra == 9:
+        x = 600
+        y = 200
+    elif index_letra == 10:
+        x = 800
+        y = 200 
+    elif index_letra == 11:
+        x = 1000
+        y = 200
+    elif index_letra == 12:
+        x = 0
+        y = 400
+    elif index_letra == 13:
+        x = 200
+        y = 400
+    elif index_letra == 14:
+        x = 400
+        y = 400
+    elif index_letra == 15:
+        x = 600
+        y = 400 
+    elif index_letra == 16:
+        x = 800
+        y = 400
+    elif index_letra == 17:
+        x = 1000
+        y = 400
+    elif index_letra == 18:
+        x = 0
+        y = 600
+    elif index_letra == 19:
+        x = 200
+        y = 600 
+    elif index_letra == 20:
+        x = 400
+        y = 600
+    elif index_letra == 21:
+        x = 600
+        y = 600
+    elif index_letra == 22:
+        x = 800
+        y = 600
+    elif index_letra == 23:
+        x = 1000
+        y = 600
+    elif index_letra == 24:
+        x = 0
+        y = 800 
+    elif index_letra == 25:
+        x = 200
+        y = 800
+    elif index_letra == 26:
+        x = 400
+        y = 800
+
+    #TECLAS
+    alto = 200
+    ancho = 200
+    grueso = 3
+    
+    if letra_blanco is True:
+        cv2.rectangle(teclado,(x + grueso , y + grueso), (x + ancho - grueso, y + alto - grueso), (255,255,255), -1)
+    else:
+        cv2.rectangle(teclado,(x + grueso , y + grueso), (x + ancho - grueso, y + alto - grueso), (0,255,0), grueso)
+
+    #FORMATO DE LAS TECLAS
+    escala_letra = 10
+    letra_grueso = 4
+    tamanio = cv2.getTextSize(letra, cv2.FONT_HERSHEY_PLAIN, escala_letra, letra_grueso)[0]
+    texto_ancho , texto_alto = tamanio[0], tamanio[1]
+    texto_x = int((ancho - texto_ancho)/2) + x
+    text_y = int((alto + texto_alto)/2) + y
+    cv2.putText(teclado, letra, (texto_x,text_y), cv2.FONT_HERSHEY_PLAIN, escala_letra, (0,255,0), letra_grueso)
+    
+
+for i in range(27):
+    if i == 5:
+        letra_blanco = True
+    else:
+        letra_blanco = False
+    letras(i, key_caps[i], letra_blanco)
+
+
 
 cv2.imshow("Teclado",teclado)
 cv2.waitKey(0)
